a0b8e6dd1e34d14cf62dca5d3ce7be89a13b3307
khendersonwood <99926470+khendersonwood@users.noreply.github.com>
2023-05-09 10:28:54 -0700

    may 9 edits
    MAY 9 edits

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+// Get the audio element
+const audio = document.querySelector(".lyrics audio");
+
+// Add click event listener to the div with class "lyrics"
+document.querySelector(".lyrics").addEventListener("click", () => {
+  audio.currentTime = 0; // reset audio to beginning
+  audio.play();
+});
